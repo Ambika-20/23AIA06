fd1e6d92d7bc4a55f11a07e84d49b26f31a218a9
Ambika-20 <ambikasoundharan@gmail.com>
2026-06-30 12:36:59 +0530

    allocated weights to identify priority

--- notification-app-fe/src/utils/priorityInbox.js
+++ notification-app-fe/src/utils/priorityInbox.js
@@ -1,2 +1,18 @@
 //setting priority for messages 
 import {getTopNotifications} from "../utils/priorityInbox";
+import {minHeap} from "../utils/minHeap";
+
+const weights = {
+    placement: 3,
+    result: 2,
+    event: 1
+}
+
+function compare(a,b)
+{
+    const weightA = weights[a.type] || 0;
+    const weightB = weights[b.type] || 0;
+    if(weightA!=weightB)
+        return weightB - weightA;
+    return new Date(b.timestamp) - new Date(a.timestamp);
+}
