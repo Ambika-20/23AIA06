6ba2ac33998d481701874cef5668ab65cb1d2104
Ambika-20 <ambikasoundharan@gmail.com>
2026-06-30 12:08:03 +0530

    added basic insert, remove operations in minHeap.js

--- notification-app-fe/src/utils/minHeap.js
+++ notification-app-fe/src/utils/minHeap.js
@@ -4,4 +4,29 @@ export default class MinHeap {
         this.heap = [];
         this.compare = compare;
     }
+
+    size()
+    {
+        return this.heap.length;
+    }
+
+    peek()
+    {
+        return this.heap[0];
+    }
+
+    insert(value)
+    {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    remove()
+    {
+        if (this.heap.length === 1) return this.heap.pop();
+        const root = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return root;
+    }
 }
