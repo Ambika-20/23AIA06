f53606595ca7259fd8de26501ad51adc7a63dd85
Ambika-20 <ambikasoundharan@gmail.com>
2026-06-30 12:27:02 +0530

    added heapifyUp and heapifyDown in minHeap.js

--- notification-app-fe/src/utils/minHeap.js
+++ notification-app-fe/src/utils/minHeap.js
@@ -29,4 +29,41 @@ export default class MinHeap {
         this.heapifyDown();
         return root;
     }
+
+    heapifyUp()
+    {
+        let index = this.heap.length-1;
+        while(index>0)
+        {
+            const parentIndex = Math.floor((index-1)/2);
+            if(this.compare(this.heap[index], this.heap[parentIndex])>=0)
+                break;
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            index = parentIndex;
+        }
+    }
+
+    heapifyDown()
+    {
+        let index = 0;
+        while(true)
+        {
+            let smallest = index;
+            let left = 2*index+1;
+            let right = 2*index+2;
+
+            if(left<this.heap.length && this.compare(this.heap[left],this.heap[smallest])<0)
+                smallest = left;
+            if (right<this.heap.length && this.compare(this.heap[right],this.heap[smallest])<0)
+                smallest = right;
+            if (smallest === index) break;
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+
+    toArray()
+    {
+        return [...this.heap];
+    }
 }
